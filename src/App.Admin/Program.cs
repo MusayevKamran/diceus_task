faf15edfab307f66390c3f686e9a81e5e68621f0
kamranmusayev <k.musayev@platform.live>
2023-08-20 21:07:23 +0400

    Refactoring

--- src/App.Admin/Program.cs
+++ src/App.Admin/Program.cs
@@ -1,20 +1,61 @@
+using App.Admin.Configurations;
+using App.Infrastructure.CrossCutting.Identity.Configurations;
+using App.Infrastructure.CrossCutting.IoC;
+using App.Infrastructure.Persistence.Configurations;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
-namespace App.Admin
-{
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+var builder = WebApplication.CreateBuilder(args);
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
-    }
+builder.Configuration
+    .SetBasePath(builder.Environment.ContentRootPath)
+    .AddJsonFile("appsettings.json", true, true)
+    .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", true, true)
+    .AddEnvironmentVariables();
+
+builder.Services.AddAppDatabaseSetup(builder.Configuration);
+
+builder.Services.AddIdentityDatabaseSetup(builder.Configuration);
+
+// ASP.NET Identity Settings
+builder.Services.AddIdentitySetup();
+
+// MVC Settings
+builder.Services.AddControllersWithViews();
+            
+builder.Services.AddRazorPages();
+
+// .NET Native DI Abstraction
+builder.Services.AddProjectSetup();
+
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+    app.UseDatabaseErrorPage();
 }
+else
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.MapRazorPages();
+
+app.Run();
--- src/App.Api/Program.cs
+++ src/App.Api/Program.cs
@@ -1,20 +1,63 @@
+using App.Api.Configurations;
+using App.Infrastructure.CrossCutting.Identity.Configurations;
+using App.Infrastructure.CrossCutting.IoC;
+using App.Infrastructure.Persistence.Configurations;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
-namespace App.API
-{
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+var builder = WebApplication.CreateBuilder(args);
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
-    }
+builder.Configuration
+    .SetBasePath(builder.Environment.ContentRootPath)
+    .AddJsonFile("appsettings.json", true, true)
+    .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", true, true)
+    .AddEnvironmentVariables();
+
+
+// Setting DBContexts
+builder.Services.AddAppDatabaseSetup(builder.Configuration);
+builder.Services.AddIdentityDatabaseSetup(builder.Configuration);
+
+// WebAPI Config
+builder.Services.AddControllers();
+            
+// Identity Settings
+builder.Services.AddIdentitySetup(builder.Configuration);
+
+// Project Settings
+builder.Services.AddProjectSetup();
+
+builder.Services.AddSwaggerSetup();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
 }
+
+app.UseHttpsRedirection();
+
+app.UseRouting();
+
+app.UseCors(c =>
+{
+    c.AllowAnyHeader();
+    c.AllowAnyMethod();
+    c.AllowAnyOrigin();
+});
+
+app.UseAuthorization();
+app.UseAuthentication();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllers();
+});
+
+app.UseSwaggerSetup();
+
+app.Run();
